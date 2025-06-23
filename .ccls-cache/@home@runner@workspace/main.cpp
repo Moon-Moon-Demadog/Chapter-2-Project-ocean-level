869b6786b7c05a5c4cda87cef4237b7bbffc5e88
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-06-23 02:31:42 +0000

    finished with Miles per Tank calculation

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 int main ()
 {
-  double oceanLevel, riseRate;
-  int fiveYears, sevenYears, tenYears;
-  oceanLevel = 2133.6;
-  riseRate = 1.5;
-  fiveYears = oceanLevel + (riseRate * 5);
-  sevenYears = oceanLevel + (riseRate * 7);
-  tenYears = oceanLevel + (riseRate * 10);
-  
-    cout << "Our current ocean level of " << oceanLevel << "mm is experiencing a steady rise of " << riseRate << "mm per year."<< endl;
-  cout << "Within five years from now the new ocean level will be "<< fiveYears << "mm." << endl;
-  cout << "Seven years from now the ocean level will be "<< sevenYears << "mm." << endl;
-  cout << "And just ten years from now the ocean level will be " << tenYears << "mm." << endl;
+double CityMiles, HighwayMiles, City, Highway;
+  int Gallons = 20;
+  City = 23.5;
+  Highway = 28.9;
+  CityMiles = Gallons * City;
+  HighwayMiles = Gallons * Highway;
+  Gallons = 20;
+
+  cout << "Lets figure the efficency of a car on the highway vs in the city." << endl;
+  cout << "An average car holds " << Gallons << " gallons and gets " << City << "mpg and the highway " << Highway << "mpg." << endl;
+  cout << " So traveling on the highway you can expect to go " << HighwayMiles << "miles." << endl;
+    cout << " Now traveling in the city you can expect to go " << CityMiles << "miles." << endl;
+  cout << "Making highway travel the best option!";
   return 0;
 }
