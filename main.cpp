bb06db841bbd2397612a29c548cf4cb7354d36b4
Moon-Moon-Demadog <lmw2385@email.vccs.edu>
2025-06-23 03:46:07 +0000

    error correcting complete

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
-//Calculating the perposed rise of the ocean level in  mm over a set period of time.
+//How many customers consume energy drinks.
 #include <iostream>
 using namespace std;
 
 int main ()
 {
+double EnergyDrink, CitrusFlavor, CitrusLover;
+  long customers;
+  EnergyDrink = 2475;
+  CitrusFlavor = 58;
+  CitrusLover = EnergyDrink * CitrusFlavor/100;
+  customers = 16500;
 
+  cout << "A store surveyed " << customers << " on what drinks they enjoy." << endl;
+  cout << "Approximately " << EnergyDrink << " customers imbibe in energy drinks." << endl;
+  cout << "Of those, " << CitrusFlavor << "% love citrus flavored energy drinks." << endl;
+  cout << "A total of " << CitrusLover << " customers drink the citrus energy drinks." << endl;
+    return 0;
 }
